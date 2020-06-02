ea4aae600a9f208373957e6fa6f5ea9916dde3d5
Capi15 <capicapi1515@gmail.com>
2020-06-02 10:44:56 +0100

    passo 1 feito

--- app/src/main/java/com/example/mymuseum/activities/PagamentoOnline_1_Activity.kt
+++ app/src/main/java/com/example/mymuseum/activities/PagamentoOnline_1_Activity.kt
@@ -4,11 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import com.example.mymuseum.R
 
 class PagamentoOnline_1_Activity : AppCompatActivity() {
+
+    private lateinit var nome: EditText
+    private lateinit var email: EditText
+    private lateinit var telemovel: EditText
+    private lateinit var morada: EditText
+    private lateinit var codigoPostal: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +25,21 @@ class PagamentoOnline_1_Activity : AppCompatActivity() {
             findViewById<View>(R.id.pagamentoOnline_1Id) as ConstraintLayout
         //define a cor do Layout
         currentLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.newBackground))
+        nome = findViewById(R.id.pagamento_online_fat_nome_text)
+        email = findViewById(R.id.pagamento_online_fat_email_text)
+        telemovel = findViewById(R.id.pagamento_online_fat_telemovel_text)
+        morada = findViewById(R.id.pagamento_online_fat_morada_text)
+        codigoPostal = findViewById(R.id.pagamento_online_fat_codigo_postal_text)
+
     }
 
     fun goToStep2(view: View) {
-        val intent = Intent(this, PagamentoOnline_2_Activity::class.java)
-        startActivity(intent)
+        if(nome.text.isEmpty() || email.text.isEmpty() || telemovel.text.isEmpty() || morada.text.isEmpty() || codigoPostal.text.isEmpty()){
+            Toast.makeText(this, "Por favor preencha todos os campos", Toast.LENGTH_SHORT).show()
+        }else{
+            val intent = Intent(this, PagamentoOnline_2_Activity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
