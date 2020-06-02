dc4be96a199e3b2316aa140f0f0dc3d5090706f4
Capi15 <capicapi1515@gmail.com>
2020-06-02 09:36:37 +0100

    falta verificar se estão checados no inicio

--- app/src/main/java/com/example/mymuseum/ui/comprarbilhete/ComprarBilheteFragment.kt
+++ app/src/main/java/com/example/mymuseum/ui/comprarbilhete/ComprarBilheteFragment.kt
@@ -3,7 +3,6 @@ package com.example.mymuseum.ui.comprarbilhete
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,12 +15,12 @@ import com.example.mymuseum.R
 class ComprarBilheteFragment : Fragment() {
 
     var totalPagar: TextView? = null
-    private var myPagarEditText: EditText? = null
     var strPagar: String = ""
     var dblPagar: Double = 0.00
-    var strBilhete: String = ""
     var dblBilhete : Double = 0.00
     var dblMuseu : Double = 0.00
+    var novoPagar: Double = 0.00
+    var result: Double = 1.00
 
     lateinit var myQuantidadeEditText: EditText
     private lateinit var limpar: Button
@@ -45,8 +44,10 @@ class ComprarBilheteFragment : Fragment() {
         myQuantidadeEditText = root.findViewById(R.id.compraB_quantidade_input)
         totalPagar = root.findViewById(R.id.compraB_valor_total)
         limpar = root.findViewById(R.id.compraB_limpar_button)
+
         radioGroup1 = root.findViewById(R.id.compraB_grupo_museus)
         radioGroup2 = root.findViewById(R.id.compraB_grupo_bilhetes)
+
         radioM1 = root.findViewById(R.id.compraB_radiobutton_museu_1)
         radioM2 = root.findViewById(R.id.compraB_radiobutton_museu_2)
         radioM3 = root.findViewById(R.id.compraB_radiobutton_museu_3)
@@ -54,30 +55,52 @@ class ComprarBilheteFragment : Fragment() {
         radioB2 = root.findViewById(R.id.compraB_radiobutton_tipo_2)
         radioB3 = root.findViewById(R.id.compraB_radiobutton_tipo_3)
 
-        radioGroup1.setOnClickListener{
-            val radioId = radioGroup1.checkedRadioButtonId
-            radioGroup1 = root.findViewById(radioId)
-            when (radioGroup1){
-                radioB1 -> dblMuseu = 0.30
-                radioB2 -> dblMuseu = 0.40
-                radioB3 -> dblMuseu = 0.20
+        radioGroup1.setOnCheckedChangeListener{ radioGroup: RadioGroup, checkedId: Int ->
+            val radioButton = root.findViewById<RadioButton>(checkedId)
+            dblMuseu = when (checkedId) {
+                radioM1.id -> {
+                    0.30
+                }
+                radioM2.id -> {
+                    0.40
+                }
+                radioM3.id -> {
+                    0.20
+                }
+                else -> {
+                    0.00
+                }
             }
+            Toast.makeText(activity, dblMuseu.toString(), Toast.LENGTH_SHORT).show()
         }
 
-        radioGroup2.setOnClickListener {
-            val radioId = radioGroup2.checkedRadioButtonId
-            radioGroup2 = root.findViewById(radioId)
-            when (radioGroup2.checkedRadioButtonId){
-                R.id.compraB_radiobutton_tipo_1 -> dblBilhete = 1.75
-                R.id.compraB_radiobutton_tipo_2 -> dblBilhete = 3.00
-                R.id.compraB_radiobutton_tipo_3 -> dblBilhete = 4.15
+        radioGroup2.setOnCheckedChangeListener{ radioGroup: RadioGroup, checkedId: Int ->
+            val radioButton = root.findViewById<RadioButton>(checkedId)
+              when (checkedId) {
+                radioB1.id -> {
+                    dblBilhete = 1.75
+//                    novoPagar = result * (dblBilhete + dblMuseu)
+//                    strPagar = novoPagar.toString()
+//                    totalPagar!!.text = strPagar
+                }
+                radioB2.id -> {
+                    dblBilhete = 3.00
+//                    novoPagar = result * (dblBilhete + dblMuseu)
+//                    strPagar = novoPagar.toString()
+//                    totalPagar!!.text = strPagar
+                }
+                radioB3.id -> {
+                    dblBilhete = 4.15
+//                    novoPagar = result * (dblBilhete + dblMuseu)
+//                    strPagar = novoPagar.toString()
+//                    totalPagar!!.text = strPagar
+                }
+                else -> {
+                    dblBilhete = 0.00
+                }
             }
+            Toast.makeText(activity, dblBilhete.toString(), Toast.LENGTH_SHORT).show()
         }
-
-        dblPagar = dblMuseu + dblBilhete
-        Log.i("log", dblPagar.toString())
-        totalPagar!!.text = dblPagar.toString()
-        Log.i("log", totalPagar!!.text.toString())
 
         limpar.setOnClickListener {
             myQuantidadeEditText.setText("")
@@ -86,30 +109,24 @@ class ComprarBilheteFragment : Fragment() {
 
         myQuantidadeEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-
             }
 
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                strPagar = myQuantidadeEditText.text.toString()
+                val quantidade: String = myQuantidadeEditText.text.toString()
+                if(!quantidade.isEmpty()){
+                    dblPagar = quantidade.toDouble()
+                    result = dblPagar * (dblBilhete + dblMuseu)
+                    Toast.makeText(activity, result.toString(), Toast.LENGTH_SHORT).show()
+                }else{
+                    result = dblBilhete + dblMuseu
+                }
+                strPagar = result.toString()
                 totalPagar!!.text = strPagar
-                Log.i("log2", strPagar)
             }
 
             override fun afterTextChanged(s: Editable) {
             }
         })
-
-        Log.i("log1", myQuantidadeEditText.text.toString())
         return root
-    }
-
-    //atualiza o valor do total
-    fun totalAPagar(){
-        totalPagar = activity?.findViewById(R.id.compraB_valor_total)
-        myPagarEditText = activity?.findViewById(R.id.compraB_quantidade_input)
-        if(totalPagar!!.text != myPagarEditText?.text){
-            totalPagar!!.text = myPagarEditText?.text.toString()
-
-        }
     }
 }
